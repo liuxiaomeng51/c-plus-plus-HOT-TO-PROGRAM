8f53e83b6faefa29241f481e73aee26fb82cb2e6
DoraXmon <23089351+liuxiaomeng51@users.noreply.github.com>
2023-11-27 17:18:09 -0500

    first commit

---
+++ chapter1/print.cpp
@@ -0,0 +1,13 @@
+//Code for Fig. 2.1: fig02_01.cpp
+//This is a program printing one line text
+
+#include <iostream> //allow program to output data to the screen
+
+// function main begins program execution
+int main()
+{
+	std::cout <<"Welcome to C++!\n"; // display message
+	
+	return 0;  // indicate that program ended successfully
+
+} // end function main
